00996465b0b15b6a66dbf015e72e5d8b5a3f88c6
Airyque Ervin <airyque@gmail.com>
2017-04-10 19:21:46 -0700

    Control song behavior

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,36 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+    this.on('add', this.enqueue, this);
+    this.on('dequeue', this.dequeue, this);
+    this.on('ended', this.next, this);
+  },
 
+  enqueue: function() {
+    if (this.length === 1) {
+      // console.log('THIS IN THE ENQUUEE', this);
+      this.playFirst();
+    }
+  },
+
+  dequeue: function() {
+    if (this.at(0) === this) {
+      this.next();
+    } else {
+      this.remove(this);
+    }
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  },
+
+  next: function() {
+    this.shift();
+    if (this.length >= 1) {
+      this.playFirst();
+    } else {
+      this.trigger('stop');
+    }
+  }
 });
