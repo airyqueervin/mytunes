90b163f12a9ff8f7ac35481d06671e55944f88bd
Airyque Ervin <airyque@gmail.com>
2017-04-11 10:17:22 -0700

    pass tests player view

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -6,6 +6,9 @@ var PlayerView = Backbone.View.extend({
   el: '<audio controls autoplay />',
 
   initialize: function() {
+    this.$el.on('ended', (() => {
+      this.model.ended();
+    }));
   },
 
   setSong: function(song) {
